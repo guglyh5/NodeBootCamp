71bb6355c3662b5eaba273a64330a3a579bd1c09
Jaspreet Singh <guglyh5@gmail.com>
2021-11-20 01:59:18 +0530

    Commit changes

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config/congi.env'});
 
 const app = express();
+app.get('/',(req, res)=>{
+    res.status(400).json({name:'Jaspreet'})
+});
 const PORT = process.env.PORT || 5900;
 
 app.listen(PORT, 
