2cfd89717918522bd04390f259659f986194e8b5
Jaspreet Singh <guglyh5@gmail.com>
2021-11-20 02:14:43 +0530

    Updated the complete folder structure

---
+++ controllers/bootcamps.js
@@ -0,0 +1,18 @@
+
+exports.getBootcamp = (req, res, next) =>{
+
+}
+
+
+exports.createBootcamp= (req, res, next) =>{
+
+}
+
+exports.UpdateBootcamp= (req, res, next) =>{
+
+}
+
+
+exports.DeleteBootcamp= (req, res, next) =>{
+
+}
