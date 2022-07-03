017559b9fd2359eb804359c5491ab31d9cef5ff8
Jaspreet Singh <guglyh5@gmail.com>
2022-07-04 01:15:57 +0530

    select and sort changes

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -23,14 +23,42 @@ exports.getBootcamp = asyncHandler(async (req, res, next) =>{
 // @route   GET /api/v1/bootcamps
 // @access  Public 
 exports.getBootcamps = asyncHandler(async (req, res, next) =>{
-        console.log(req.query);
         let query;
-        let queryStr = JSON.stringify(req.query);
+        //copy req.query
+        const reqQuery = { ...req.query };
+
+        //Fields to exclude
+        const removeFields = ['select'];
+        
+        //Loop from removefields and delete them from reqQuery
+        removeFields.forEach(param => delete reqQuery[param]);
+
+        //create query string
+        let queryStr = JSON.stringify(reqQuery);
+
+        //create operators ($gt, $lte etc.)
         queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-        console.log(queryStr);
+
+        //finding resource
         query = Bootcamp.find(JSON.parse(queryStr));
+
+        //Select fields
+        if(req.query.select){
+        const fields = req.query.select.split(',').join(' ');
+        query = query.select(fields);
+        }
+
+        //Sort fields
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+            }else{
+                query=query.sort('-created_at');
+            }
+
+        //executing query
         const bootcamps = await query;
-        // const count = await Bootcamp.count();
+
         res
         .status(200)
         .json({success: true, count: bootcamps.length, data: bootcamps});
